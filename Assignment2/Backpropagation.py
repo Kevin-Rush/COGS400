83cbcb9eb3967c687c665a8caccf290c318cd779
Kevin Rush <kevinrush.net@gmail.com>
2020-10-24 01:07:08 -0400

    Stable at 90+ % accuracy

--- Assignment2/Backpropagation.py
+++ Assignment2/Backpropagation.py
@@ -15,28 +15,21 @@ class BackpropNN():
         self.input_size = 784
         self.hidden_size = 25
         self.output_size = 10
-        self.learning_rate = 0.75
+        self.learning_rate = 0.5
         self.a = 0.05
         self.bias = 1
         self.matrix = np.zeros((10, 10))
         self.count = 0
         self.MSE = 9999
         self.avrg_MSE = 0
+        self.iterations = 5
 
         self.weights_input_hidden_last_weight_change = np.zeros((self.input_size+1,self.hidden_size), dtype=float)
         self.weights_hidden_output_last_weight_change = np.zeros((self.hidden_size, self.output_size), dtype=float)
 
-        #weights
-        #self.IH_test = open(file_input_hidden, "r")
-        #self.HO_test = open(file_hidden_output)
-        self.weights_input_hidden = 2*np.random.random((self.input_size+1, self.hidden_size))-1 #784x7 weight matrix from input to hidden layer
-        self.weights_hidden_output = 2*np.random.random((self.hidden_size, self.output_size))-1 #7x10 weight matrix from hidden to output layer
+        self.weights_input_hidden = 2*np.random.random((self.input_size+1, self.hidden_size))-1 
+        self.weights_hidden_output = 2*np.random.random((self.hidden_size, self.output_size))-1 
 
-        #self.weights_input_hidden = np.empty((self.input_size +  1, self.hidden_size))
-        #self.weights_hidden_output = np.empty((self.hidden_size, self.output_size))
-
-        #self.weights_input_hidden = np.ones((self.input_size +  1, self.hidden_size))
-        #self.weights_hidden_output = np.ones((self.hidden_size, self.output_size))
 
     def sigmoid(self, input):
         return 1/(1 + np.exp(-input))
@@ -58,7 +51,6 @@ class BackpropNN():
         one_array = np.zeros(10)
         largest = -1
         index = -1
-        #print(array.shape)
         for i in range(len(array)):
             if largest < array[i]:
                 largest = array[i]
@@ -77,91 +69,47 @@ class BackpropNN():
         self.MSE = actual_mse/len(mse)
         self.avrg_MSE = (self.avrg_MSE+self.MSE)/self.count 
 
-
-    def feedforward (self, X):                                  #forward propogation through the network
-
+    def feedforward (self, X):                                  
         
-        self.z = np.dot(X.T, self.weights_input_hidden)         #dot product of input matrix and first set of weights, returns 784x7 matrix
-        self.z2 = self.sigmoid(self.z)                          #activation function, returns the z matrix through the sigmoid function
+        self.z = np.dot(X.T, self.weights_input_hidden)         
+        self.z2 = self.sigmoid(self.z)                          
         
-        self.z3 = np.dot(self.z2, self.weights_hidden_output)   #dot product of hidden layer (z2) and second set of weights, returns 784x10 matrix
+        self.z3 = np.dot(self.z2, self.weights_hidden_output)   
         output = self.sigmoid(self.z3)
-        """
-        print("Z: "+str(self.z))
-        print("Z2: "+str(self.z2))
-        print("Z3: "+str(self.z3))
-        print("output: "+str(output))
-        print("-------------------------------------------------------------------")
-        """
+
         return output
 
-    def backpropogation(self, X, y, output):                                    #backward propogate through the network
+    def backpropogation(self, X, y, output):                          #backward propogate through the network
         
         self.loss_fn(y, output)
         self.output_error = (y-output)
         output_sig_der = self.sigmoid_derivaitve(output)
-        output_change = self.learning_rate*self.output_error*output_sig_der
+        output_change = self.output_error*output_sig_der
         output_delta = np.outer(output_change, self.z2) 
-        
 
-        error_matrix = self.output_error*output_sig_der*self.weights_hidden_output
-        delta_error_sum_matrix = np.zeros((len(error_matrix)))
-        for i in range(len(error_matrix)):
-            delta_error_sum_matrix[i] = np.sum(error_matrix[i])
-        """
-        print(delta_error_sum_matrix)   
-        print(delta_error_sum_matrix)
-        print(delta_error_sum_matrix.shape)
-        print("Old:")
-        print(np.sum(error_matrix))
-        exit()
-        """
-        z2_change = self.learning_rate*delta_error_sum_matrix*self.sigmoid_derivaitve(self.z2)             #Multiplying learning rate, with delta_error_sum and the sigmoid derivative of z2
-        z2_delta = np.outer(z2_change, X)
+        error_matrix = self.weights_hidden_output*output_change
+        error_matrix = np.sum(error_matrix, axis=1)
 
-        self.weights_hidden_output += output_delta.T + self.a*self.weights_hidden_output_last_weight_change  #ajdusting the second set (hidden --> output) weights
-        self.weights_input_hidden += z2_delta.T + self.weights_input_hidden_last_weight_change
+        hidden_sig_der = self.sigmoid_derivaitve(self.z2)
+        errors_multiplied = error_matrix*hidden_sig_der
+        z2_delta = np.outer(errors_multiplied, X)
+
+        self.weights_hidden_output += self.learning_rate*output_delta.T + self.a*self.weights_hidden_output_last_weight_change  
+        self.weights_input_hidden += self.learning_rate*z2_delta.T + self.a*self.weights_input_hidden_last_weight_change
 
         self.weights_hidden_output_last_weight_change = output_delta.T + self.a*self.weights_hidden_output_last_weight_change
-        self.weights_input_hidden_last_weight_change = z2_delta.T + self.weights_input_hidden_last_weight_change   #adjusting first set (input --> hidden) weights
-
-        """
-        print("MSE: "+str(self.MSE))
-        print("output_eror: "+str(self.output_error))
-        print("output sig der: " +str(output_sig_der))        
-        print("output delta: "+str(output_delta))
-        print("-------------------------------------------------------------------")
-        print("sum_delta: "+str(delta_error_sum))
-        print("sig_der_z2: "+str(self.sigmoid_derivaitve(self.z2)))
-        print("z2 delta"+str(z2_delta))
-        print("-------------------------------------------------------------------")
-        print("weights:")
-        print(self.weights_hidden_output)
-        print(self.weights_input_hidden)
-        print()
-        print(self.weights_hidden_output_last_weight_change)
-        print(self.weights_input_hidden_last_weight_change)
-        print("-------------------------------------------------------------------")
-        print("-------------------------------------------------------------------")
-
-        self.count += 1
-        if (self.count == 3):
-            exit()
-        """
+        self.weights_input_hidden_last_weight_change = z2_delta.T + self.a*self.weights_input_hidden_last_weight_change  
 
     def train(self, X, y):
-        array = np.zeros((len(y), 10))
+        array = np.zeros((len(y), self.output_size))
         for i in range(len(y)):
             array[i][y[i]] = 1
 
         j = 0
-        while (j < 5) and (self.MSE > 0.05):
-        #for j in range(5):                                  #iteratior
+        for j in range(self.iterations)                                  #iterator
             for i in range(len(X)):
-                image = X[i].reshape(784,1)
-
-                #print(image)
-                image = np.append(image, [[1]])
+                image = X[i].reshape(self.input_size,1)
+                image = np.append(image, [[self.bias]])
                 self.count += 1
                 output = self.feedforward(image)             #sending one 28x28 matrix that represents an image
                 self.backpropogation(image, array[i], output)    #y is a single digit   
@@ -175,17 +123,16 @@ class BackpropNN():
         correct = 0
         incorrect = 0
         for i in range(len(X)):
-            array = np.zeros((len(y), 10))              #one-hot encoding the actual output
+            array = np.zeros((len(y), self.output_size))              #one-hot encoding the actual output
             for j in range(len(y)):
                 array[j][y[j]] = 1
-            image = X[i].reshape(784,1)
-            image = np.append(image, [[1]])             #add in the bias
+            image = X[i].reshape(self.input_size,1)
+            image = np.append(image, [[self.bias]])             #add in the bias
             output = self.feedforward(image)
             output = self._actual_pred(output)           #get an exact number as output
             index_output = self.find_index_largest(output)
             if index_output == self.find_index_largest(array[i]):   #compare if the index's match up
                 correct += 1
-                #self.matrix[index_output][index_output] += 1
             else:
                 incorrect += 1
 
@@ -193,18 +140,6 @@ class BackpropNN():
 
 if __name__ == "__main__":      #main function to run program and generate output for display purposes
     
-    f_hidden_output = "C:/Users/Kevin/Documents/My University/Queen's/Courses/CISC_COGS/COGS 400/COGS400/Assignment2/hidden_output_weights.txt"
-    f_input_hidden = "C:/Users/Kevin/Documents/My University/Queen's/Courses/CISC_COGS/COGS 400/COGS400/Assignment2/input_hidden_weights.txt"
-
-    """
-    T = []
-    with open(f_hidden_output, "r") as file1:
-        for line in file1.readlines():
-            f_list = [float(i) for i in line.split("]") if i.strip()]
-            T += f_list[7:11]
-    print(T)
-    exit()
-    """
     NN = BackpropNN()
 
     (train_X, train_y), (test_X, test_y) = tf.keras.datasets.mnist.load_data() #returns an array of 28 x 28 images
@@ -217,28 +152,4 @@ if __name__ == "__main__":      #main function to run program and generate outpu
 
     NN.train(train_X, train_y)
 
-    """
-    for i in range(len(NN.weights_input_hidden)):
-        print(NN.weights_input_hidden[i])
-    print()
-    print("---------------------------------------------------------------------------")
-    print()
-    for i in range(len(NN.weights_hidden_output)):
-        print(NN.weights_hidden_output[i])
-    
-    f_IH = open(f_input_hidden, "w")
-    f_HO = open(f_hidden_output, "w")
-
-    a_file = open("test.txt", "w")
-for row in an_array:
-    np.savetxt(a_file, row)
-
-    f_IH.write("NN.weights_input_hidden")
-    f_HO.write("NN.weights_hidden_output")
-    f_IH.write(str(NN.weights_input_hidden))    
-    f_HO.write(str(NN.weights_hidden_output))
-    f_HO.close()
-    f_IH.close()
-    """
     print(NN.test(test_X, test_y))
-    #print(NN.matrix)
