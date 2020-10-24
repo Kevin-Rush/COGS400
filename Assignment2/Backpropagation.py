0b73a7fb50d57ba009afbf0e6fa3d71e6f8453f6
Kevin Rush <kevinrush.net@gmail.com>
2020-10-24 17:02:26 -0400

    Confusion matrix implemented in M1, M2 updated and working at 90% +

--- Assignment2/Backpropagation.py
+++ Assignment2/Backpropagation.py
@@ -11,25 +11,27 @@ import numpy as np
 
 class BackpropNN():
     def __init__(self): #file_input_hidden, file_hidden_output):
-        #parameters
+        #NN parameters
         self.input_size = 784
         self.hidden_size = 15
         self.output_size = 10
-        self.learning_rate = 0.5
+        self.learning_rate = 0.1
         self.alpha = 0.05
         self.bias = 1
-        self.matrix = np.zeros((10, 10))
+        #General parameters
         self.count = 0
         self.MSE = 9999
         self.alphavrg_MSE = 0
         self.iterations = 5
-
+        #weights for momentum
         self.weights_input_hidden_last_weight_change = np.zeros((self.input_size+1,self.hidden_size), dtype=float)
         self.weights_hidden_output_last_weight_change = np.zeros((self.hidden_size, self.output_size), dtype=float)
-
+        #weights 
         self.weights_input_hidden = 2*np.random.random((self.input_size+1, self.hidden_size))-1 
         self.weights_hidden_output = 2*np.random.random((self.hidden_size, self.output_size))-1 
-
+        #initialize empty comfusion matrix
+        self.test_confusion_matrix = np.zeros((self.output_size+1, self.output_size+1), dtype=int)
+        self.train_confusion_matrix = np.zeros((self.output_size+1, self.output_size+1), dtype=int)
 
     def sigmoid(self, input):
         return 1/(1 + np.exp(-input))
@@ -100,6 +102,59 @@ class BackpropNN():
         self.weights_hidden_output_last_weight_change = output_delta.T + self.alpha*self.weights_hidden_output_last_weight_change
         self.weights_input_hidden_last_weight_change = z2_delta.T + self.alpha*self.weights_input_hidden_last_weight_change  
 
+    def matrix_body_update(self, predicted_output, actual_output, test):
+        if test == False:
+            self.train_confusion_matrix[predicted_output][actual_output] +=1
+        else:
+            self.test_confusion_matrix[predicted_output][actual_output] +=1
+
+    def matrix_edge_update(self, test):
+        
+        if test == False:
+            index = 0
+            for i in range(len(self.train_confusion_matrix)-1):
+                sum = 0 
+                for j in range(len(self.train_confusion_matrix[i] - 1) - 1):
+                    sum += self.train_confusion_matrix[i][j]
+                
+                percentage = (self.train_confusion_matrix[i][index]/sum)*100
+                self.train_confusion_matrix[i][len(self.train_confusion_matrix)-1] = percentage
+                index += 1
+
+            index = 0
+            for j in range(len(self.train_confusion_matrix[i] - 1) - 1):
+                
+                sum = 0
+                for i in range(len(self.train_confusion_matrix)-1):
+                    sum += self.train_confusion_matrix[i][j]
+                    
+                percentage = (self.train_confusion_matrix[index][j]/sum)*100
+                self.train_confusion_matrix[len(self.train_confusion_matrix)-1][j] = percentage
+                index += 1
+        else:
+            index = 0
+            for i in range(len(self.test_confusion_matrix)-1):
+                sum = 0 
+                for j in range(len(self.test_confusion_matrix[i] - 1) - 1):
+                    sum += self.test_confusion_matrix[i][j]
+                
+                percentage = (self.test_confusion_matrix[i][index]/sum)*100
+                self.test_confusion_matrix[i][len(self.test_confusion_matrix)-1] = percentage
+                index += 1
+
+            index = 0
+            for j in range(len(self.test_confusion_matrix[i] - 1) - 1):
+                
+                sum = 0
+                for i in range(len(self.test_confusion_matrix)-1):
+                    sum += self.test_confusion_matrix[i][j]
+                    
+                percentage = (self.test_confusion_matrix[index][j]/sum)*100
+                self.test_confusion_matrix[len(self.test_confusion_matrix)-1][j] = percentage
+                index += 1
+            
+            
+
     def train(self, X, y):
         array = np.zeros((len(y), self.output_size))
         for i in range(len(y)):
@@ -114,9 +169,19 @@ class BackpropNN():
                 output = self.feedforward(image)             #sending one 28x28 matrix that represents an image
                 self.backpropogation(image, array[i], output)    #y is a single digit   
 
+                #one_hot_output = self._actual_pred(output)           #get an exact number as output
+                predicted_output = self.find_index_largest(output)
+                actual_output = self.find_index_largest(array[i])
+                self.matrix_body_update(predicted_output, actual_output, False)
+
             j += 1
             print("J: "+str(j))
-            print("MSE: "+str(self.MSE))  
+            print("MSE: "+str(self.MSE))
+            self.matrix_edge_update(False)
+            print(self.train_confusion_matrix)
+        
+        self.matrix_edge_update(False)
+  
     
     def test(self, X, y):
 
@@ -129,14 +194,20 @@ class BackpropNN():
             image = X[i].reshape(self.input_size,1)
             image = np.append(image, [[self.bias]])             #add in the bias
             output = self.feedforward(image)
+            
             output = self._actual_pred(output)           #get an exact number as output
-            index_output = self.find_index_largest(output)
-            if index_output == self.find_index_largest(array[i]):   #compare if the index's match up
+            predicted_output = self.find_index_largest(output)
+            actual_output = self.find_index_largest(array[i])
+            self.matrix_body_update(predicted_output, actual_output, True)
+            #print(self.test_confusion_matrix)
+            if predicted_output == actual_output:   #compare if the index's match up
                 correct += 1
             else:
                 incorrect += 1
 
         return correct, incorrect
+
+            
 
 if __name__ == "__main__":      #main function to run program and generate output for display purposes
     
@@ -152,4 +223,8 @@ if __name__ == "__main__":      #main function to run program and generate outpu
 
     NN.train(train_X, train_y)
 
+    print("Train Confusion Matrix: ")
+    print(NN.train_confusion_matrix)
     print(NN.test(test_X, test_y))
+    print("Test Confusion Matrix: ")
+    print(NN.test_confusion_matrix)
--- Assignment2/Backpropagation_Model2.py
+++ Assignment2/Backpropagation_Model2.py
@@ -1,31 +1,40 @@
-import tensorflow as tf
-from tensorflow import keras
 import numpy as np
+import tensorflow as tf
+#import mnist 
+#import matplotlib.plyplot as plt
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.utils import to_categorical
 
 (train_X, train_y), (test_X, test_y) = tf.keras.datasets.mnist.load_data() #returns an array of 28 x 28 images
-    
+
 train_X = tf.keras.utils.normalize(train_X, axis=1)
 test_X = tf.keras.utils.normalize(test_X, axis=1)
 
+train_X = train_X.reshape((-1, 784))
+test_X = test_X.reshape((-1, 784))
+
 input_size = 784
-hidden_size = 100
-output_size = 1
+hidden_size = 25 
+output_size = 10
 
-NN = keras.Sequential()
-NN.add(tf.keras.layers.Dense(hidden_size, input_dim=input_size, activation="sigmoid"))
-NN.add(tf.keras.layers.Dense(output_size, activation="sigmoid"))
+NN = Sequential()
+NN.add(Dense(hidden_size, activation="sigmoid", input_dim=input_size))
+NN.add(Dense(output_size, activation="sigmoid"))
 
-NN.compile(loss="mean_squared_error", optimizer="adam", metrics=["accuracy"])
+NN.compile(optimizer="adam", loss="mean_squared_error", metrics=["accuracy"])
+NN.fit(train_X, to_categorical(train_y), epochs = 10)
 
-train_X = train_X.reshape(train_X.shape[0], train_X.shape[1]*train_X.shape[2])
-train_y = tf.one_hot(train_y, 60000, on_value=1.0, off_value=0.0,)
+NN.fit(test_X, to_categorical(test_y), epochs = 1)
 
-NN.fit(train_X, train_y, epochs=20)
-
+predictions = NN.predict(test_y[:5])
+print(predictions)
+"""
 loss, accuracy = NN.evaluate(test_X, test_y)
 
 print("Loss: "+str(loss))
 print("Accuracy: "+str(accuracy))
+"""
 """
 #Confution Matrix and Classification Report
 Y_pred = NN.predict_generator(validation_generator, num_of_test_samples // batch_size+1)
